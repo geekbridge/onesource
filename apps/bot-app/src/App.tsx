8ec192ede22a061918269adc63fb7d24e6a0512f
Sergey Gutskal <gutskalss@gmail.com>
2023-10-22 17:57:43 +0300

    feat(bot-app): init telegram web app api

--- apps/bot-app/src/App.tsx
+++ apps/bot-app/src/App.tsx
@@ -1,14 +1,26 @@
-import { Button } from 'ui'
+import { tg } from './constants'
 
 import viteLogo from '/vite.svg'
 import './App.css'
 
 export function App() {
+  if (!tg) {
+    return (
+      <div>
+        <h1>Telegram is not detected</h1>
+      </div>
+    )
+  }
+
+  tg.BackButton.show()
+  tg.BackButton.onClick(tg.close)
+
   return (
     <div>
       <img src={viteLogo} class='logo' alt='Vite logo' />
       <h1>OneSource Mini App</h1>
-      <Button preset='styled'>UI Button</Button>
+      <button onClick={() => tg.close()}>Close</button>
+      <div>api version: {tg.version}</div>
     </div>
   )
 }
