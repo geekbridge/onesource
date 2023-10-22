4748737435a2c1f19e493e1011bd8b397baad8b9
Sergey Gutskal <gutskalss@gmail.com>
2023-10-22 17:57:43 +0300

    feat(bot-app): init telegram web app api

--- apps/bot-app/src/App.tsx
+++ apps/bot-app/src/App.tsx
@@ -1,14 +1,27 @@
 import { Button } from 'ui'
 
+import { tg } from './constants'
+
 import viteLogo from '/vite.svg'
 import './App.css'
 
 export function App() {
+  if (!tg) {
+    return (
+      <div>
+        <h1>Telegram is not detected</h1>
+      </div>
+    )
+  }
+
+  tg.BackButton.show()
+
   return (
     <div>
       <img src={viteLogo} class='logo' alt='Vite logo' />
       <h1>OneSource Mini App</h1>
-      <Button preset='styled'>UI Button</Button>
+      <Button onClick={tg.close}>Close</Button>
+      <div>api version: {tg.version}</div>
     </div>
   )
 }
--- apps/bot-app/src/index.tsx
+++ apps/bot-app/src/index.tsx
@@ -1,9 +1,16 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
+import { Telegram } from '@twa-dev/types'
 
 import { App } from './App'
 
 import './index.css'
+
+declare global {
+  interface Window {
+    Telegram: Telegram
+  }
+}
 
 const root = document.getElementById('root')
 
