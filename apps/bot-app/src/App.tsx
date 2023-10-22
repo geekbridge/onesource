516afe23a7082c6b63ae0d0006a1c8dfd4242f8a
Sergey Gutskal <gutskalss@gmail.com>
2023-10-22 17:56:15 +0300

    feat(bot-app): init mini app with solidjs+ts+vite

---
+++ apps/bot-app/src/App.tsx
@@ -0,0 +1,11 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export function App() {
+  return (
+    <div>
+      <img src={viteLogo} class='logo' alt='Vite logo' />
+      <h1>OneSource Mini App</h1>
+    </div>
+  )
+}
